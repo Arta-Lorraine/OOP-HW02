b8602e62938a3c8fec091760ee06bcf697602308
fcu-d0426524 <jackyarta@gmail.com>
2016-10-18 16:19:24 +0800

    Add files via upload

--- src/StringComparator.java
+++ src/StringComparator.java
@@ -15,7 +15,14 @@ public class StringComparator {
 			System.out.println("The two strings are the same.");
 		}
 		else{
+			while (str1.equalsIgnoreCase(str2)==false)
+			{
 			System.out.println("The two strings are not the same.");
+			System.out.print("Enter a String1: ");
+			str1 = scanner.next();
+			System.out.print("Enter a String2: ");
+			str2 = scanner.next();
+			}
 		}
 	}
 
